c6a4e39b93ef725f3dd5a5eca43975a62d10b7fd
Janusz Kamienski <janusz.kamienski@deployed.pl>
2018-08-25 18:47:38 +0200

    little fixes to front

--- front/src/pages/CharacterCreate.js
+++ front/src/pages/CharacterCreate.js
@@ -60,7 +60,7 @@ export class SelectField extends React.Component {
           value={value}
           onChange={onChange}
           inputProps={{
-            name: {name},
+            name: `${name}`,
             id: `${name}-select`,
           }}
         >
@@ -68,7 +68,7 @@ export class SelectField extends React.Component {
             <MenuItem value=''>None</MenuItem>
           )}
           {options.map(option => (
-            <MenuItem value={option.id}>{option.name}</MenuItem>
+            <MenuItem key={`select-${name}-{$option-id}`} value={option.id}>{option.name}</MenuItem>
           ))}
         </Select>
       </FormControl>
